2a7730d48bec4b7c5291d3d2482e6e1b1029c869
Darren Henderson <darrenh@Darrens-MacBook-Pro.local>
2020-05-17 23:42:32 -0500

    Ran php linter.

--- app/Console/Commands/WordOfTheDay.php
+++ app/Console/Commands/WordOfTheDay.php
@@ -38,49 +38,48 @@ class WordOfTheDay extends Command
      */
     public function handle()
     {
-      // $words = [
-      //     'aberration' => 'a state or condition markedly different from the norm',
-      //     'convivial' => 'occupied with or fond of the pleasures of good company',
-      //     'diaphanous' => 'so thin as to transmit light',
-      //     'elegy' => 'a mournful poem; a lament for the dead',
-      //     'ostensible' => 'appearing as such but not necessarily so'
-      // ];
-      //
-      // // Finding a random word
-      // $key = array_rand($words);
-      // $value = $words[$key];
-      //
-      // // $users = User::all();
-      // $users = [
-      //   ['name' => 'Ester Diaz', 'email' => 'ediaz@ccsj.edu'],
-      //   ['name' => 'Sam Smith', 'email' => 'ssmith@ccsj.edu'],
-      // ];
+        // $words = [
+        //     'aberration' => 'a state or condition markedly different from the norm',
+        //     'convivial' => 'occupied with or fond of the pleasures of good company',
+        //     'diaphanous' => 'so thin as to transmit light',
+        //     'elegy' => 'a mournful poem; a lament for the dead',
+        //     'ostensible' => 'appearing as such but not necessarily so'
+        // ];
+        //
+        // // Finding a random word
+        // $key = array_rand($words);
+        // $value = $words[$key];
+        //
+        // // $users = User::all();
+        // $users = [
+        //   ['name' => 'Ester Diaz', 'email' => 'ediaz@ccsj.edu'],
+        //   ['name' => 'Sam Smith', 'email' => 'ssmith@ccsj.edu'],
+        // ];
 
-      // $user = {'name' => 'Ester Diaz', 'email' => 'ediaz@ccsj.edu'};
-      //
-      // dd($user);
+        // $user = {'name' => 'Ester Diaz', 'email' => 'ediaz@ccsj.edu'};
+        //
+        // dd($user);
 
-      // Mail::raw("{$key} -> {$value}", function ($mail) use ($user) {
-      //         $mail->from('info@tutsforweb.com');
-      //         $mail->to($user->email)
-      //             ->subject('Word of the Day');
-      //     });
+        // Mail::raw("{$key} -> {$value}", function ($mail) use ($user) {
+        //         $mail->from('info@tutsforweb.com');
+        //         $mail->to($user->email)
+        //             ->subject('Word of the Day');
+        //     });
 
-          Mail::raw('Test email', function($message)
-          {
+        Mail::raw('Test email', function ($message) {
             $message->from('darrenh@ccsj.edu', 'Darren H')
                 ->to('ediaz@ccsj.edu', 'Ester Diaz')
                 ->subject('Email command test');
-          });
+        });
 
-      // foreach ($users as $user) {
-      //     Mail::raw("{$key} -> {$value}", function ($mail) use ($user) {
-      //         $mail->from('info@tutsforweb.com');
-      //         $mail->to($user->email)
-      //             ->subject('Word of the Day');
-      //     });
-      // }
+        // foreach ($users as $user) {
+        //     Mail::raw("{$key} -> {$value}", function ($mail) use ($user) {
+        //         $mail->from('info@tutsforweb.com');
+        //         $mail->to($user->email)
+        //             ->subject('Word of the Day');
+        //     });
+        // }
 
-      $this->info('Word of the Day sent to All Users');
+        $this->info('Word of the Day sent to All Users');
     }
 }
