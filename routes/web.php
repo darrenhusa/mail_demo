0c0c97798a66a1fc73f82b43f7f4b7190bbf1923
Darren Henderson <darrenh@ccsj.edu>
2020-06-08 12:55:48 -0500

    Working on query builder ideas. Think I solved the one field lookup problem!!!!  Need help adding in one join to give me the AT query that I need for the email report.

--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,7 @@ Route::get('/example2', function () {
            'CCSJ_PROD.CCSJ_CO_V_NAME.NAME_ID', '=',
            'CCSJ_PROD.SR_STUDENT_TERM.NAME_ID')
     ->where('TERM_ID', '=', '20201')
-    ->where('DFLT_ID', '=', '123456789');
+    ->where('DFLT_ID', '=', '100124780');
 
     $sql = $query->toSql();
     $results = $query->get();
@@ -47,6 +47,81 @@ Route::get('/example2', function () {
     // return view('welcome');
 });
 
+Route::get('/lookup-at-athlete-status', function () {
+
+    $termId = '20201';
+
+    //beyonce hampton - does not have any 20201 record!!!
+    $studentId = '100124780';
+    // count = 0
+
+    // Kevin McCune has TWO AT-sports! MBO and MBS!!!
+    $studentId = '100110307';
+    // count = 2
+
+    // Hannah Gentry has ONE AT-sport WSO!!!
+    $studentId = '100116464';
+    // count = 1
+
+    // Andres Gomez is a PB - this is a non-athlete sport!!!
+    // Need to join the remaining table to FILTER out records like this one!
+
+
+    //Access query builder results
+    // SELECT
+    //     CCSJ_PROD_AT_ADMI_TERM.TERM_ID,
+    //     CCSJ_PROD_AT_ADMI_TERM.ADST_ID,
+    //     CCSJ_PROD_AT_ADMI_TERM.ETYP_ID,
+    //     CCSJ_PROD_CCSJ_CO_V_NAME.DFLT_ID,
+    //     CCSJ_PROD_CCSJ_CO_V_NAME.LAST_NAME,
+    //     CCSJ_PROD_CCSJ_CO_V_NAME.FIRST_NAME,
+    //     CCSJ_PROD_AT_ADMI_ACTIV.ACTI_ID,
+    //     CCSJ_PROD_CO_ACTIV_CODE.ATHLETIC_FLAG
+    // FROM ((CCSJ_PROD_CCSJ_CO_V_NAME
+    // INNER JOIN
+    // CCSJ_PROD_AT_ADMI_TERM ON
+    // CCSJ_PROD_CCSJ_CO_V_NAME.NAME_ID =
+    // CCSJ_PROD_AT_ADMI_TERM.NAME_ID)
+    // INNER JOIN
+    // CCSJ_PROD_AT_ADMI_ACTIV ON
+    // CCSJ_PROD_AT_ADMI_TERM.NAME_ID =
+    // CCSJ_PROD_AT_ADMI_ACTIV.NAME_ID)
+    // INNER JOIN
+    // CCSJ_PROD_CO_ACTIV_CODE ON
+    // CCSJ_PROD_AT_ADMI_ACTIV.ACTI_ID =
+    // CCSJ_PROD_CO_ACTIV_CODE.ACTI_ID
+    // WHERE (((CCSJ_PROD_AT_ADMI_TERM.TERM_ID)="20201") AND
+    // ((CCSJ_PROD_CCSJ_CO_V_NAME.DFLT_ID)="100124780") AND
+    // ((CCSJ_PROD_CO_ACTIV_CODE.ATHLETIC_FLAG)="T"));
+
+    $query = \DB::connection('odbc')
+    ->table('CCSJ_PROD.AT_ADMI_TERM')
+    // ->select('TERM_ID', 'ADST_ID', 'ETYP_ID', 'DFLT_ID', 'LAST_NAME', 'FIRST_NAME', 'ACTI_ID', 'ATHLETIC_FLAG')
+    ->select('TERM_ID', 'ADST_ID', 'ETYP_ID', 'DFLT_ID', 'LAST_NAME', 'FIRST_NAME', 'ACTI_ID')
+    ->where('TERM_ID', '=', $termId)
+    ->join('CCSJ_PROD.CCSJ_CO_V_NAME', function ($join) {
+        $join->on('CCSJ_PROD.CCSJ_CO_V_NAME.NAME_ID', '=', 'CCSJ_PROD.AT_ADMI_TERM.NAME_ID');
+    })
+    // ->where('DFLT_ID', '=', $studentId)
+    // ->where('ACTI_ID', '=', 'PB')
+    ->join('CCSJ_PROD.AT_ADMI_ACTIV',
+            'CCSJ_PROD.AT_ADMI_TERM.NAME_ID', '=',
+            'CCSJ_PROD.AT_ADMI_ACTIV.NAME_ID');
+    // ->join('CCSJ_PROD.AT_ADMI_ACTIV',
+    //         'CCSJ_PROD.AT_ADMI_TERM.ACTI_ID', '=',
+    //         'CCSJ_PROD.CCSJ_PROD_CO_ACTIV_CODE.ACTI_ID');
+
+    $sql = $query->toSql();
+    // $numAtSports = $query->count();
+    $results = $query->get();
+    $count = $query->count();
+
+    dd($sql, $count);
+    // dd($numAtSports, $results);
+    // dd($sql, $results, $count);
+
+    // return view('welcome');
+});
 
 
 //Send Fall 2020 FtTradHeadcountByTypes Email (to mailtrap.io)
