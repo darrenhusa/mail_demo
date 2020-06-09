4a9a6dbfbb3c30ead85226c4a90e4563ab2cfb50
Darren Henderson <darrenh@ccsj.edu>
2020-06-09 10:58:08 -0500

    Played with tailwind css formatting.

--- resources/views/trad_headcount/index.blade.php
+++ resources/views/trad_headcount/index.blade.php
@@ -5,33 +5,36 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>TRAD Headcount Report</title>
+  <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
   <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"> -->
 </head>
 <body>
-  <h1>Fall 2020 Full-time TRAD Enrolled Students</h1>
-  <table border='1'>
-    <theader>
-      <tr>
-        <td>Student ID</td>
-        <td>Last</td>
-        <td>First</td>
-        <td>Entry Type</td>
-        <td>Is AT or SR Athlete</td>
-      </tr>
-    </theader>
-    <tbody>
-      @foreach($students as $student)
-      <tr>
-        <td>{{ $student->DFLT_ID }}</td>
-        <td>{{ $student->LAST_NAME }}</td>
-        <td>{{ $student->FIRST_NAME }}</td>
-        <td>{{ $student->EntryTypeAlt }}</td>
-        <td>{{ $student->IsAthlete }}</td>
-      </tr>
-      @endforeach
-    </tbody>
-  </table>
-  {{ $students->links() }}
+  <h1 class="bg-teal-200">Fall 2020 Full-time TRAD Enrolled Students</h1>
+  <div class="table-auto pl-3 bg-gray-300">
+    <table>
+      <theader class="italic">
+        <tr>
+          <td>Student ID</td>
+          <td>Last</td>
+          <td>First</td>
+          <td>Entry Type</td>
+          <td>Is AT or SR Athlete</td>
+        </tr>
+      </theader>
+      <tbody>
+        @foreach($students as $student)
+        <tr>
+          <td>{{ $student->DFLT_ID }}</td>
+          <td>{{ $student->LAST_NAME }}</td>
+          <td>{{ $student->FIRST_NAME }}</td>
+          <td>{{ $student->EntryTypeAlt }}</td>
+          <td>{{ $student->IsAthlete }}</td>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
+    {{ $students->links() }}
+  </div>
 </body>
 </html>
